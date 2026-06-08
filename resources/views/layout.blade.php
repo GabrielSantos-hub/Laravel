a91f69379171b20ab27aabba366ddd0ba95216dc
Gabriel <Hamburguer117@gmail.com>
2026-06-08 16:04:31 -0300

    favicon adicionado

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,12 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GUEASS - Gerador de Prompts</title>
+    
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;600;700;900&display=swap" rel="stylesheet">
-
+   
     <style>
         body {
             background-color: #ffffff;
